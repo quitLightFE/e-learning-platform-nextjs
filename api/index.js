a64be48e9cc8894d5b4fe1d38f57750c0adb9ee7
quitLightFE <quitlight101@gmail.com>
2026-02-06 17:43:07 +0500

    changed domain

--- api/index.js
+++ api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://e-learning-platform-nextjs.vercel.app/api",
 });
 
 const getStudents = async () => {
